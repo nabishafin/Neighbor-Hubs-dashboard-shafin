f4c4ee2a1181e155ad766057d7953e7c3ddc3c50
nabishafin <shafin21215@gmail.com>
2025-07-20 04:18:44 +0600

    vite config file error solved

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    allowedHosts: ["darling-pangolin-totally.ngrok-free.app"], // ✅ Add this line
+  },
 });
